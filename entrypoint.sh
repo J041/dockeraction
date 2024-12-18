b06908b92f04ebbc8845fcf02aebbb7736a54576
Joel <46340016+J041@users.noreply.github.com>
2024-12-18 15:36:05 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
-curl=curl https://www.postb.in/1734507018287-9273768381681
-wget=wget https://www.postb.in/1734507018287-9273768381681
+curl https://www.postb.in/1734507018287-9273768381681
+wget https://www.postb.in/1734507018287-9273768381681?hello=world
