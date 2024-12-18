17f0e934f7d179e1ccd6d89564987a0a6eea209e
Joel <46340016+J041@users.noreply.github.com>
2024-12-18 15:50:16 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
 echo "time=$time" >> test
-output=$test | base64
+output=$($test | base64)
 # wget https://www.postb.in/1734507018287-9273768381681?hello=world
 wget https://www.postb.in/1734507018287-9273768381681?$output
