fb15025dd3f8d11c838c5de05acbed69dcd2ae7e
Joel <46340016+J041@users.noreply.github.com>
2024-12-18 15:26:18 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
-
+curl=$(whereis curl)
+echo "curl=$curl" >> $GITHUB_OUTPUT
+wget=$(whereis wget)
+echo "wget=$wget" >> $GITHUB_OUTPUT
